e10dc34a9c31ccd58c48a808efcf02d69c3eeb11
unknown <saiswaroopasaranath1211@gmail.com>
2023-06-08 21:22:01 +0530

    ADD: StopWatch - Start,Stop,Reset,Resume

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+    const [timer, setTimer] = useState(0);
+    const [running, setRunning] = useState(false);
+    const [pausedTime, setPausedTime] = useState(0);
+    const [intervalId, setIntervalId] = useState(null);
+  
+    const handleStart = () => {
+      if (!running) {
+        setRunning(true);
+        const id = setInterval(() => {
+          setTimer(prevTime => prevTime + 1);
+        }, 500);
+        setIntervalId(id);
+      }
+    };
+  
+    const handlePause = () => {
+      if (running) {
+        setRunning(false);
+        clearInterval(intervalId);
+        setPausedTime(timer);
+      }
+    };
+  
+    const handleReset = () => {
+      setTimer(0);
+      setRunning(false);
+      clearInterval(intervalId);
+      setPausedTime(0);
+    };
+  
+    const lap = () => {
+      // Implement lap functionality here
+    };
+  
+    const handleResume = () => {
+      if (!running && pausedTime > 0) {
+        setRunning(true);
+        const id = setInterval(() => {
+          setTimer(prevTime => prevTime + 1);
+        }, 500);
+        setIntervalId(id);
+        setPausedTime(0);
+      }
+    };
+  
+    const formatTime = () => {
+      const seconds = `0${timer % 60}`.slice(-2);
+      const minutes = `0${Math.floor((timer / 60) % 60)}`.slice(-2);
+      const hours = `0${Math.floor(timer / 3600)}`.slice(-2);
+  
+      return `${hours} : ${minutes} : ${seconds}`;
+    };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="timer-Display">
+         {/* {time} */}
+         {formatTime()}
+      </div>
+      {/* <ul className="laps"></ul> */}
+      <div className="buttons">
+        <button className="startTimer" onClick={handleStart}>Start</button>
+        <button className="pauseTimer" onClick={handlePause}>Pause</button>
+        <button className="resetTimer" onClick={handleReset}>Reset</button>
+        <button className="restartTimer" onClick={handleResume}>Resume</button>
+        {/* <button className="lap" onclick={lap}>Lap</button> */}
+        {/* <button className="resetLap" onclick={resetLap}>Reset Lap</button> */}
+
+      </div>
     </div>
   );
 }
